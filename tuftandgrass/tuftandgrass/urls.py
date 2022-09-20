37d63edf88add3f880ef64dee60a0dc74888c0c8
alexagapkin <alexthemaggot23@gmail.com>
2022-09-20 23:46:02 +0500

    add summernote url

--- tuftandgrass/tuftandgrass/urls.py
+++ tuftandgrass/tuftandgrass/urls.py
@@ -21,6 +21,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('summernote/', include('django_summernote.urls')),
     path('', include('mainapp.urls', namespace='mainapp')),
     path('uz/', include('mainapp.urls_uz', namespace='mainapp_uz')),
     path('en/', include('mainapp.urls_en', namespace='mainapp_en')),
