b0b91196dc119feca4a8521a95ffae1a073b304e
alexagapkin <alexthemaggot23@gmail.com>
2022-09-20 17:21:10 +0500

    add news detail url

--- tuftandgrass/mainapp/urls.py
+++ tuftandgrass/mainapp/urls.py
@@ -7,6 +7,7 @@ app_name = 'mainapp'
 urlpatterns = [
     path('', views.index, name='index'),
     path('news/', views.news_list, name='news_list'),
+    path('news/<int:post_id>/', views.news_detail, name='news_detail'),
     path('order/', views.order, name='order'),
     path('subscribe/', views.subscribe, name='subscribe'),
 ]
--- tuftandgrass/mainapp/urls_uz.py
+++ tuftandgrass/mainapp/urls_uz.py
@@ -7,4 +7,5 @@ app_name = 'mainapp_uz'
 urlpatterns = [
     path('', views.index, name='index'),
     path('news/', views.news_list, name='news_list'),
+    path('news/<int:post_id>/', views.news_detail, name='news_detail'),
 ]
