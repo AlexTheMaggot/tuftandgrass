9e4fba1506be36ab8d1067bacab3271b6113d75a
alexagapkin <alexthemaggot23@gmail.com>
2022-09-24 23:43:39 +0500

    fix debug mode and allowed hosts

--- tuftandgrass/tuftandgrass/settings.py
+++ tuftandgrass/tuftandgrass/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = DJANGO_SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = DJANGO_DEBUG
 
-ALLOWED_HOSTS = ['*', ]
+ALLOWED_HOSTS = DJANGO_ALLOWED_HOSTS
 
 
 # Application definition
